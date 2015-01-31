2951685c3dcef576ae166018d3ee3018fe262aca
Huzefa Fatakdawala <hafatakd@uwaterloo.ca>
2015-01-31 18:38:15 -0500

    Fixed login.

--- routes/login.js
+++ routes/login.js
@@ -4,13 +4,15 @@ var router = express.Router();
 var Accountdata = require('../models/accountdata').Accountdata;
 
 /* Login Attempt */
-router.get('/', function(req, res) {
+router.post('/', function(req, res) {
     var uname = req.body.username;
-    var pword = req.body.passord;
-    Accountdata.find({ username: uname, passord: pword }, function(err, docs) {
-        if(!err) {
-            if (docs.size >= 0){
-                res.json(200, { message: docs }); 
+    var pword = req.body.password;
+    Accountdata.find({ username: uname, password: pword }, function(err, docs) {
+    //Accountdata.findOne({ "username": uname, "password": pword }, function(err, docs) {
+       // console.log(docs, JSONObject objects = getArray.getJSONArray(i););
+	if(!err) {
+            if (docs.length > 0){
+                res.json(200, { message: docs[0] }); 
             } else {
                 res.json(200, { message: "No Login Data" });  
             }

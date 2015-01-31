6492a2d20ea7ef146e61bfdaacc13d9634616959
Dhairyasarous <dp-1994@hotmail.com>
2015-01-31 16:57:14 -0500

    question_data schema updated

--- models/questiondata.js
+++ models/questiondata.js
@@ -2,9 +2,11 @@ var mongoose = require('mongoose')
   , Schema = mongoose.Schema;
       
 var questiondataSchema = new Schema({
-    name : { type: String, required: true, trim: true, index: { unique: true } }
-  , description : { type: String, required: true }
-  , date_created : { type: Date, required: true, default: Date.now}
+    q_id : { type: String, required: true, trim: true, index: { unique: true } },
+    question : { type: String, required: true, trim: true },
+    yes : { type: Number, required: true, trim: true },
+    no: { type: Number, required: true, trim: true },
+    timestamp : { type: Date, required: true, default: Date.now }
 });
       
 var questiondata = mongoose.model('questiondata', questiondataSchema);

8034868c2003fdc4fec67bc5e3a33a1b88fa851e
Matthew Pitropov <matthewpitropov@gmail.com>
2015-02-01 06:12:56 +0000

    completed updatequestion api

--- routes/updatequestion.js
+++ routes/updatequestion.js
@@ -1,39 +1,52 @@
 var express = require('express');
 var router = express.Router();
 
-var Questiondata = require('../models/questiondata').Questiondata;
+var QuestionData = require('../models/questiondata').QuestionData;
 var Answers = require('../models/answers').Answers;
 
-/* Post QuestionID into user's list of Answered Questions
-   And update the question with the users choice          */
-router.post('/updatequestion', function(req, res) {
+
+router.post('/', function(req, res) {
     var uname = req.body.username;
     var questionAskedId = req.body.questionId;
+    var Answer = req.body.answer;
 
-  Questiondata.findOne({ question: { $regex: new RegExp(questionAsked, "i") } }, function(err, doc) {
-    if(!err && !doc) {
-      
-      var newQuestiondata = new Questiondata(); 
-      newQuestiondata.name = questionAsked;
-      
-      newQuestiondata.save(function(err) {
-
-        if(!err) {
-          res.json(201, {message: "Question was created: " + newQuestiondata.id });    
-        } else {
-          res.json(500, {message: "Could not create question. Error: " + err});
-        }
-
-      });
-
-    } else if(!err) {
-      // User Question already exists 
-      res.json(403, {message: "Question Already Exists."}); 
-
+    var query = { q_id : questionAskedId};
+    var newData;
+    if (Answer === 'Yes') {
+        newData = {$inc: {"yes": 1}};
     } else {
-      res.json(500, { message: err});
-    } 
-  });
+        newData = {$inc: {"no": 1}};
+    }
+    
+    var yes, no;
+    QuestionData.findOneAndUpdate(query, {$inc: {"yes": 1}}, {upsert:false}, function(err, doc){
+        if (err) return res.send(500, { error: "Question does not exist" });
+        //return res.send("succesfully saved");
+        yes = doc.yes;
+        no = doc.no;
+    });
+    
+    //append the q_id of the question object to the questions array
+      var query = Answers.where({ username: uname });
+      query.findOne(function(err,docs) {
+        if (docs) {
+          var q_ids = docs.q_ids;
+          q_ids.push(questionAskedId);
+          query.update({ $set: { q_ids: questionAskedId }},function(err){
+            if(!err)
+              res.json(201, {message: "Question placed in Answer list: " + questionAskedId + " No: "+no+" Yes: "+yes}); 
+          });
+        } else {
+          var newAnswersObject = new Answers();
+          newAnswersObject.username = uname;
+          newAnswersObject.q_ids = [questionAskedId];
+          newAnswersObject.save(function(err) {
+            if(!err)
+              res.json(201, {message: "Question placed in new Answer list: " + questionAskedId + " No: "+no+" Yes: "+yes}); 
+          });
+        }            
+      });
+    
 });
 
 module.exports = router;

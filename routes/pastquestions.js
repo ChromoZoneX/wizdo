bbd4222de074f591ec7ddf3b6273e1220ba6e37b
Andy Leung <a.leung1994@gmail.com>
2015-02-01 08:03:17 -0500

    error checking on answers.html

--- routes/pastquestions.js
+++ routes/pastquestions.js
@@ -2,14 +2,15 @@ var express = require('express');
 
 var router = express.Router();
 
-var questions = require('../models/questions').Questions;
+// var questions = require('../models/questions').Questions;
+var QuestionData = require('../models/questiondata').QuestionData;
 
 router.post('/', function(req, res) {
 	var uname = req.body.username;	
-	questions.find({ username : uname }, function(err, docs) {
+	QuestionData.find({ username : uname }, function(err, docs) {
 		if(!err) {
 			if(docs.length > 0)
-				res.json( 200, { message : docs[0] });
+				res.json( 200, { message : docs });
 			else
 				res.json( 200, { message : 'Empty' });
 		} else {

aacc4f9030d49af867bb8c010f966cce628b95f1
Matthew Pitropov <matthewpitropov@gmail.com>
2015-02-01 00:42:28 +0000

    Added end points

--- routes/answer.js
+++ routes/answer.js
@@ -6,12 +6,40 @@ var Answers = require('../models/answers').Answers;
 
 /* Retrieve a Question */
 router.get('/retrievequestion', function(req, res) {
-
-    var newQuestiondata = new Questiondata();
-    console.log("1");
     
-    rand = Math.random();
-    Questiondata.find().limit(-1).skip(rand).next();
+    //var newQuestiondata = new Questiondata();
+    
+    //res.json(200, { "data" : "newQuestiondata" });
+    
+    var bigDoc;
+    var query = Questiondata.find({}, function(err, doc){
+        bigDoc = doc;
+        if(!err && doc !== null) {
+            //do nothing
+        } else {
+          //res.json(500, {message: "Could not get question. Error: " + err});
+        } 
+    });
+    
+    var rand = Math.floor((Math.random() * bigDoc.size));
+        console.log("1");
+    while ($.inArray(bigDoc[rand][0], categories) >= 0){ //returns -1 if not found
+        rand = Math.floor((Math.random() * 10));
+    }
+
+    
+    res.json(200, {message: bigDoc[rand]});
+    
+    //.limit(-1).skip(rand).next();
+    /*
+    console.log(query);
+    if ( query === null ){
+        res.json(500, { message: "No Questions :("});
+    }
+    else{
+        res.json(200, { data : query });
+    }
+    
     console.log("2");
     newQuestiondata.find({ q_ids: QuestionData.q_id }).select({q_ids: QuestionData.q_id});
     console.log("3");
@@ -20,7 +48,8 @@ router.get('/retrievequestion', function(req, res) {
     }
     else {
         res.json(500, { message: "No Questions :("});
-    }
+    }*/
+    
 });
 
 /* Post QuestionID into user's list of Answered Questions */
